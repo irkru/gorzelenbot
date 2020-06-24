5d6c218113e502370b24e5b9ee35d18c61bc9e16
Леонид Демьянов <45462831+De17eon@users.noreply.github.com>
2020-06-25 00:13:37 +0800

    Add files via upload

---
+++ GorZelenBot/gbot/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class TreeModel(models.Model):
+    sender = models.CharField(max_length=100)
+    text = models.CharField(max_length=512)
+    create_time = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now=True)
+    latitude = models.CharField(max_length=100)
+    longitude = models.CharField(max_length=100)
+    img1 = models.ImageField(upload_to='uploads/TreeModel/', null=True, default=None)
+    img2 = models.ImageField(upload_to='uploads/TreeModel/', null=True, default=None)
+    img3 = models.ImageField(upload_to='uploads/TreeModel/', null=True, default=None)
+    img4 = models.ImageField(upload_to='uploads/TreeModel/', null=True, default=None)
+
