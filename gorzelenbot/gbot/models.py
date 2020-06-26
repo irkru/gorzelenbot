6f8dae7f011da68f75ecabdf6cf6e5055c1f4376
Леонид Демьянов <45462831+De17eon@users.noreply.github.com>
2020-06-26 15:42:24 +0800

    Добавил UserModel

--- gorzelenbot/gbot/models.py
+++ gorzelenbot/gbot/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 
 
+class UserModel(models.Model):
+    user_id = models.PositiveIntegerField()
+    is_bot = models.BooleanField()
+    first_name = models.CharField(max_length=32)
+    username = models.CharField(max_length=32)
+    last_name = models.CharField(max_length=32)
+    language_code = models.CharField(max_length=5)
+
+
+
 class TreeModel(models.Model):
     sender = models.CharField(max_length=100)
     text = models.CharField(max_length=512)
