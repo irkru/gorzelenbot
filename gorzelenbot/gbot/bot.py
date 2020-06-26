e45c7b8b73153bf9c3373a25604d1ba938f8d7f4
Леонид Демьянов <45462831+De17eon@users.noreply.github.com>
2020-06-26 17:29:52 +0800

    Добавил заполнение данных о новых пользователях в базу данных

--- gorzelenbot/gbot/bot.py
+++ gorzelenbot/gbot/bot.py
@@ -1,8 +1,9 @@
 import httplib2
-
+import sys
 import telebot
 from django.conf import settings
 
+from .models import UserModel
 
 bot = telebot.TeleBot(settings.BOT_TOKEN)
 
@@ -21,18 +22,35 @@ def start_message(message):
     5) Отправьте команду /regend (Добавлю позднее)
     """
     bot.send_message(message.chat.id, mes)
-    keyboard = telebot.types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
-    button_geo = telebot.types.KeyboardButton(text="Отправить местоположение", request_location=True)
-    keyboard.add(button_geo)
+
+    cu_id = message.from_user.id
+
+    if not len(UserModel.objects.filter(user_id=cu_id)):
+        cu_is_bot = message.from_user.is_bot
+
+        if not cu_is_bot:
+
+            cu_username = message.from_user.username if (message.from_user.username != None) else ''
+            cu_first_name = message.from_user.first_name if (message.from_user.first_name != None) else ''
+            cu_last_name = message.from_user.last_name if (message.from_user.last_name != None) else ''
+            cu_language_code = message.from_user.language_code if (message.from_user.language_code != None) else ''
+
+            current_user = UserModel(
+                user_id=cu_id,
+                is_bot=cu_is_bot,
+                first_name=cu_first_name,
+                username=cu_username,
+                last_name=cu_last_name,
+                language_code=cu_language_code
+            )
+            current_user.save()
 
 
 @bot.message_handler(content_types=['text'])
 def send_text(message):
-    if message.text.lower() == 'привет':
-        bot.send_message(message.chat.id, 'Привет, бос)')
-    elif message.text.lower() == 'пока':
-        bot.send_message(message.chat.id, 'Прощай, создатель')
-    print(message.text)
+    print('*** dir(message) :', dir(message))
+    print('*** message :', message)
+    print('*** message.text :', message.text)
 
 
 @bot.message_handler(content_types=['photo'])
