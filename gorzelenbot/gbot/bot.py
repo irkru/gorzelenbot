347589e940cee2d026994466e14e0cd97b4e10ca
Dmitry P <nonix.diman@gmail.com>
2020-07-01 16:09:39 +0800

    рефакторинг
    - получение юзера и дерева в каждой функции
    - сообщения в отдельном файле
    - download_imagefield в модель перенесли

--- gorzelenbot/gbot/bot.py
+++ gorzelenbot/gbot/bot.py
@@ -1,12 +1,13 @@
 import telebot
 from telebot import types
-import tempfile
+
 import urllib.request
 import os
 
 from django.core.files import File
 from django.conf import settings
 from .models import UserModel, TreeModel
+from .messages import *
 
 bot = telebot.TeleBot(settings.BOT_TOKEN)
 
@@ -15,156 +16,137 @@ def get_url_image(tkn, image_path):
     return 'https://api.telegram.org/file/bot{}/{}'.format(tkn, image_path)
 
 
-def save_img_from_url_to_imagefield(imgfield, url):
-    img_name = os.path.basename(url)
-    img_temp = tempfile.TemporaryFile()
-    img_temp.write(urllib.request.urlopen(url).read())
-    img_temp.flush()
-    imgfield.save(img_name, File(img_temp))
+def update_or_create_user(user):
+    """
+    Создает пользователя, если его нет в базе данных, или обновляет его данные,
+    если он есть
+    """
+    update = {
+        'is_bot': user.is_bot,
+        'first_name': value_or_empty(user.first_name),
+        'last_name': value_or_empty(user.last_name),
+        'username': user.username,
+        'language_code': user.language_code,
+
+    }
+    obj, created = UserModel.objects.update_or_create(
+        user_id=user.id,
+        defaults=update,
+    )
+
+    return obj
+
+
+def get_or_create_tree(user):
+    """
+    Возвращает последнее дерево пользователя, или создает новое
+    """
+    return TreeModel.objects.get_or_create(sender=user, status='C')
+
+
+def value_or_empty(value):
+    return value if value else ''
 
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    cu_id = message.from_user.id
 
     # Проверка на бота
     cu_is_bot = message.from_user.is_bot
     if cu_is_bot:
         return
 
-    # Проверяем есть ли пользователь в базе.
-    # Если нету создаём нового.
-    # Записиваем его в перменную current_user.
-    if not len(UserModel.objects.filter(user_id=cu_id)):
-        cu_username = message.from_user.username if (message.from_user.username != None) else ''
-        cu_first_name = message.from_user.first_name if (message.from_user.first_name != None) else ''
-        cu_last_name = message.from_user.last_name if (message.from_user.last_name != None) else ''
-        cu_language_code = message.from_user.language_code if (message.from_user.language_code != None) else ''
-        current_user = UserModel(
-            user_id=cu_id,
-            is_bot=cu_is_bot,
-            first_name=cu_first_name,
-            username=cu_username,
-            last_name=cu_last_name,
-            language_code=cu_language_code
-        )
-        current_user.save()
-    else:
-        current_user = UserModel.objects.get(user_id=cu_id)
+    # создаем или получаем пользователя
+    user = update_or_create_user(message.from_user)
+    tree, created = get_or_create_tree(user)
 
-    # Создаём <<новое дерево>>.
-    if len(TreeModel.objects.filter(sender=current_user, status='C')) < 1:
-        current_tree = TreeModel(
-            sender=current_user,
-            status='C',
-        )
-        current_tree.save()
-    else:
-        print('**** Уже есть дерево ****')
+    if not created:
+        # открытое дерево уже есть, зачем пользователь вызывает старт?
+        bot.send_message(message.chat.id, MSG_ALREADY_STARTED)
+        return
 
-    mes = """Вас приветствует бот Горзеленхоза
-
-    Для того чтобы зарегистровать больное дерево:
-
-    1) Отправьте до 4 его фотографий (Желательно с разных сторон)
-    2) Отправьте геопозицию
-    3) Отправьте комментарий, в нём следует подробно описать где находится дерево
-    4) Отправьте команду /check, чтобы проверить все ли данные заполнены
-    5) Отправьте команду /save
-    """
-    bot.send_message(message.chat.id, mes)
+    bot.send_message(message.chat.id, MSG_HELLO)
 
 
 @bot.message_handler(commands=['save'])
 def save_message(message):
-    cu_id = message.from_user.id
-    cu_user = UserModel.objects.get(user_id=cu_id)
 
-    if len(TreeModel.objects.filter(sender=cu_user, status='C')) == 1:
-        cu_tree = TreeModel.objects.get(sender=cu_user, status='C')
+    user = update_or_create_user(message.from_user)
+    tree, created = get_or_create_tree(user)
 
-        message_to_user = "Все данные приянты. Заявка принята"
+    message_to_user = MSG_SAVED.format(tree.id)
 
-
-        if cu_tree.text != "" and cu_tree.latitude != "" and cu_tree.longitude != "" and cu_tree.img1.name != "":
-            cu_tree.status = 'F'
-            cu_tree.save()
-            bot.send_message(message.chat.id, message_to_user, reply_markup=keyboard())
-        else:
-            message_to_user = cu_tree.check_fields()
-            bot.send_message(message.chat.id, message_to_user, reply_markup=keyboard())
+    if tree.text != "" and tree.latitude != "" and tree.longitude != "" and tree.img1.name != "":
+        tree.status = 'F'
+        print('save', tree.status)
+        tree.save()
+        bot.send_message(message.chat.id, message_to_user, reply_markup=keyboard())
+    else:
+        message_to_user = tree.check_fields()
+        bot.send_message(message.chat.id, message_to_user, reply_markup=keyboard())
 
 
 @bot.message_handler(content_types=['text'])
 def send_text(message):
-    cu_id = message.from_user.id
-    cu_user = UserModel.objects.get(user_id=cu_id)
 
-    if len(TreeModel.objects.filter(sender=cu_user, status='C')) == 1:
-        cu_tree = TreeModel.objects.get(sender=cu_user, status='C')
+    user = update_or_create_user(message.from_user)
+    tree, created = get_or_create_tree(user)
 
-        old_text = cu_tree.text
-        new_text = old_text + '\n' + message.text
-        cu_tree.text = new_text
-        cu_tree.save()
+    old_text = tree.text
+    new_text = old_text + '\n' + message.text
+    tree.text = new_text
+    tree.save()
 
-        if old_text == '':
-            bot.send_message(message.chat.id, 'Комментарий отправлен', reply_markup=keyboard())
-        else:
-            bot.send_message(message.chat.id, 'Комментарий дополнен', reply_markup=keyboard())
+    if not old_text:
+        bot.send_message(message.chat.id, 'Комментарий отправлен', reply_markup=keyboard())
+    else:
+        bot.send_message(message.chat.id, 'Комментарий дополнен', reply_markup=keyboard())
 
 
 @bot.message_handler(content_types=['photo'])
 def photo(message):
 
-    cu_id = message.from_user.id
-    cu_user = UserModel.objects.get(user_id=cu_id)
+    user = update_or_create_user(message.from_user)
+    tree, created = get_or_create_tree(user)
 
-    if len(TreeModel.objects.filter(sender=cu_user, status='C')) == 1:
-        cu_tree = TreeModel.objects.get(sender=cu_user, status='C')
+    file_img = bot.get_file(message.photo[-1].file_id)
+    url = get_url_image(settings.BOT_TOKEN, file_img.file_path)
 
-        fileID = message.photo[-1].file_id
-        file_img = bot.get_file(fileID)
-        cu_url = get_url_image(settings.BOT_TOKEN, file_img.file_path)
+    if not tree.img1:
+        TreeModel.download_imagefield(tree.img1, url)
+        msg = 'Фотография 1 загружена'
+    elif not tree.img2:
+        TreeModel.download_imagefield(tree.img2, url)
+        msg = 'Фотография 2 загружена'
+    elif not tree.img3:
+        TreeModel.download_imagefield(tree.img3, url)
+        msg = 'Фотография 3 загружена'
+    elif not tree.img4:
+        TreeModel.download_imagefield(tree.img4, url)
+        msg = 'Фотография 4 загружена'
+    else:
+        msg = 'Последняя фотография не была сохранена, можно сохранить только 4 фотографии'
 
-        if cu_tree.img1.name == '':
-            save_img_from_url_to_imagefield(cu_tree.img1, cu_url)
-            mes_to_user = 'Фотография 1 загружена'
-        elif cu_tree.img2.name == '':
-            save_img_from_url_to_imagefield(cu_tree.img2, cu_url)
-            mes_to_user = 'Фотография 2 загружена'
-        elif cu_tree.img3.name == '':
-            save_img_from_url_to_imagefield(cu_tree.img3, cu_url)
-            mes_to_user = 'Фотография 3 загружена'
-        elif cu_tree.img4.name == '':
-            save_img_from_url_to_imagefield(cu_tree.img4, cu_url)
-            mes_to_user = 'Фотография 4 загружена'
-        else:
-            mes_to_user = 'Последняя фотография не была сохранена, можно сохранить только 4 фотографии'
-
-        cu_tree.save()
-        bot.send_message(message.chat.id, mes_to_user, reply_markup=keyboard())
+    tree.save()
+    bot.send_message(message.chat.id, msg, reply_markup=keyboard())
 
 
 @bot.message_handler(content_types=["location"])
 def location(message):
-    cu_id = message.from_user.id
-    cu_user = UserModel.objects.get(user_id=cu_id)
+    user = update_or_create_user(message.from_user)
+    tree, created = get_or_create_tree(user)
 
-    if len(TreeModel.objects.filter(sender=cu_user, status='C')) == 1:
-        cu_tree = TreeModel.objects.get(sender=cu_user, status='C')
+    if message.location is not None:
+        if not tree.latitude and not tree.longitude:
+            msg = 'Геопозиция загружена'
+        else:
+            msg = 'Геопозиция обновлена\nМожете сохранить заявку: /save'
 
-        if message.location is not None:
-            if cu_tree.latitude == '' and cu_tree.longitude == '':
-                message_to_user = 'Геопозиция загружена'
-            else:
-                message_to_user = 'Геопозиция обновлена'
+        tree.latitude = message.location.latitude
+        tree.longitude = message.location.longitude
+        tree.save()
 
-            cu_tree.latitude = message.location.latitude
-            cu_tree.longitude = message.location.longitude
-            cu_tree.save()
-
-            bot.send_message(message.chat.id, message_to_user, reply_markup=keyboard())
+        bot.send_message(message.chat.id, msg, reply_markup=keyboard())
 
 
 def keyboard():
--- gorzelenbot/gbot/models.py
+++ gorzelenbot/gbot/models.py
@@ -1,29 +1,34 @@
+import os
+import tempfile
+import urllib.request
+
+from django.core.files import File
 from django.db import models
 
 
 class UserModel(models.Model):
-    user_id = models.PositiveIntegerField()
+    user_id = models.PositiveIntegerField(primary_key=True)
     is_bot = models.BooleanField()
-    first_name = models.CharField(max_length=32)
-    username = models.CharField(max_length=32)
-    last_name = models.CharField(max_length=32)
-    language_code = models.CharField(max_length=5)
+    first_name = models.CharField(max_length=32, blank=True)
+    username = models.CharField(max_length=32, blank=True)
+    last_name = models.CharField(max_length=32, blank=True)
+    language_code = models.CharField(max_length=5, blank=True)
 
 
 class TreeModel(models.Model):
-    VALUES_STATUS = (
+    VALUE_STATUS = (
         ('C', 'Is being created'),
         ('F', 'Filled'),
         ('P', 'Processed'),
     )
     # Самозаполняющиеся поля
     sender = models.ForeignKey(UserModel, on_delete=models.CASCADE)
-    status = models.CharField(max_length=1, choices=VALUES_STATUS, default='C')
+    status = models.CharField(max_length=1, choices=VALUE_STATUS, default='C')
     # Время. Заполняется автоматически
     create_time = models.DateTimeField(auto_now_add=True, blank=True)
     update_time = models.DateTimeField(auto_now=True, blank=True)
     # Поля для заполнения пользователем через телеграмм бот
-    text = models.CharField(max_length=512, blank=True)
+    text = models.TextField(max_length=2048, blank=True)
     latitude = models.CharField(max_length=100, blank=True)
     longitude = models.CharField(max_length=100, blank=True)
     img1 = models.ImageField(upload_to='uploads/TreeModel/', null=True, blank=True)
@@ -34,13 +39,13 @@ class TreeModel(models.Model):
     def check_fields(self):
         message_to_user = ""
 
-        if self.text == "":
+        if not self.text:
             message_to_user += "Отправьте комментарий \n"
 
-        if self.latitude == "" or self.longitude == "":
+        if not self.latitude or not self.longitude:
             message_to_user += "Отправьте геопозицию \n"
 
-        if self.img1.name == "":
+        if not self.img1:
             message_to_user += "Отправьте фотографию дерева"
 
         if message_to_user == "":
@@ -48,5 +53,9 @@ class TreeModel(models.Model):
 
         return message_to_user
 
-
-
+    def download_imagefield(imgfield, url):
+        img_name = os.path.basename(url)
+        img_temp = tempfile.TemporaryFile()
+        img_temp.write(urllib.request.urlopen(url).read())
+        img_temp.flush()
+        imgfield.save(img_name, File(img_temp))
